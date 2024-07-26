101781ccce790eab2f0ac522542b1401c1315baf
jaepil <jaepill94@gmail.com>
2024-07-26 19:26:29 +0900

    User Domain Refactor

---
+++ src/main/java/com/example/demo/user/controller/UserCreateController.java
@@ -0,0 +1,33 @@
+package com.example.demo.user.controller;
+
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserCreate;
+import com.example.demo.user.controller.response.UserResponse;
+import com.example.demo.user.infrastructure.UserEntity;
+import com.example.demo.user.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "유저(users)")
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserCreateController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserCreate userCreate) {
+        User user = userService.createUser(userCreate);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(UserResponse.from(user));
+    }
+
+}
---
+++ src/main/java/com/example/demo/user/service/UserService.java
@@ -0,0 +1,72 @@
+package com.example.demo.user.service;
+
+import com.example.demo.common.domain.exception.CertificationCodeNotMatchedException;
+import com.example.demo.common.domain.exception.ResourceNotFoundException;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
+import com.example.demo.user.domain.UserCreate;
+import com.example.demo.user.domain.UserUpdate;
+import com.example.demo.user.infrastructure.UserEntity;
+import com.example.demo.user.infrastructure.UserJpaRepository;
+import java.time.Clock;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.example.demo.user.service.port.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final CertificationService certificationService;
+
+    public User getById(long id) {
+        return userRepository.findByIdAndStatus(id, UserStatus.ACTIVE)
+                .orElseThrow(() -> new ResourceNotFoundException("Users", id));
+    }
+
+    public User getByEmail(String email) {
+        return userRepository.findByEmailAndStatus(email, UserStatus.ACTIVE)
+            .orElseThrow(() -> new ResourceNotFoundException("Users", email));
+    }
+
+    public User getByIdOrElseThrow(long id) {
+        return userRepository.findByIdAndStatus(id, UserStatus.ACTIVE)
+            .orElseThrow(() -> new ResourceNotFoundException("Users", id));
+    }
+
+    @Transactional
+    public User createUser(UserCreate userCreate) {
+        User user = User.from(userCreate);
+        user = userRepository.save(user);
+        certificationService.send(userCreate.getEmail(), user.getId(), user.getCertificationCode());
+        return user;
+    }
+
+    @Transactional
+    public User updateUser(long id, UserUpdate userUpdate) {
+        User user = userRepository.getById(id);
+        user = user.update(userUpdate);
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public void login(long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Users", id));
+        user = user.login();
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void verifyEmail(long id, String certificationCode) {
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Users", id));
+        user = user.certificate(certificationCode);
+        userRepository.save(user);
+    }
+}
